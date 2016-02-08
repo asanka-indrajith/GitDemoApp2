e5c93a324d3f0e6dcea96635658f66756a1d029b
Asanka Indrajith <asanka@geveo.com>
2016-02-08 13:59:53 +0530

    first checkin

---
+++ GitDemoApp/GitDemoApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitDemoApp.Startup))]
+namespace GitDemoApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
